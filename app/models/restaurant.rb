19a05d60e910e36389566da53639af65484914d2
Tony Turetsky <TTuretsky@gmail.com>
2018-07-06 10:58:58 -0400

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,39 @@
 class Restaurant
+
   attr_accessor :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+#reader for all restaurant instances
+  def self.all
+    @@all
+  end
+
+#searches the @@all class variable for the FIRST restaurant that matches the name arguement
+  def self.find_by_name(name)
+    self.all.find do |restaurant|
+      restaurant.name == name
+    end
+  end
+
+  #returns an array of all reviews for this instance of restaurant
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+#returns an array of customers who left reviews for this instance of restaurant
+  def customers
+    self.reviews.map do |review|
+      review.customer
+    end
+  end
+
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,33 @@
 class Review
-  
-end
 
+  attr_reader :restaurant, :content, :customer
+
+  @@all = []
+
+  def initialize(restaurant, content, customer)
+    @restaurant = restaurant
+    @content = content
+    @customer = customer
+    @@all << self
+  end
+
+#reader for all review instances
+  def self.all
+    @@all
+  end
+
+#searches all review instances for a review that has a customer instance that matches the person arguement. returns an array
+  def customer(person)
+    self.all.select do |review|
+      review.customer = person
+    end
+  end
+
+#searches all review instances for a review that has a restaurant that matces the dining_establishment arguement. returns an array
+  def restaurant(dining_establishment)
+    self.all.select do |review|
+      review.restaurant == dining_establishment
+    end
+  end
+
+end
