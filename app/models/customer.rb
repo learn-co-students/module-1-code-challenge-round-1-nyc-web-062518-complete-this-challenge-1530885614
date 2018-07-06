0c55f7b88154bb318f0d9c383405ff54f18f7025
Daniel Ran <danielr1735@gmail.com>
2018-07-06 10:53:56 -0400

    Bon Apetite
    x2;lrck;
    exit!
    
    quit

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,51 @@
 class Customer
+
+  @@all = []
+
   attr_accessor :first_name, :last_name
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find { |customer| name == "#{customer.first_name} #{customer.last_name}" }
+  end
+
+  def self.find_all_by_first_name(first_name)
+    self.all.select { |customer| first_name == customer.first_name }
+  end
+
+  def self.all_names
+    self.all.map { |customer| "#{customer.first_name} #{customer.last_name}" }
+  end
+
+  def add_review(restaurant, content='')
+    Review.new(self, restaurant, content)
+  end
+
 end
+
+
+# * Customer.all
+#   * should return **all** of the customer instances
+# * Customer.find_by_name(name)
+#   * given a string of a **full name**, returns the **first customer** whose full name matches
+# * Customer.find_all_by_first_name(name)
+#   * given a string of a first name, returns an **array** containing all customers with that first name
+# * Customer.all_names
+#   * should return an **array** of all of the customer full names
+# * Customer#add_review(restaurant, content)
+#   * given a **restaurant object** and some review content (as a string), creates a new review and associates it with that customer and restaurant. A `Review` belongs to a `Customer` and belongs to a `Restaurant`
+
+
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,26 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+# NOTE: All the restaurants here are great Hummus joints in Jerusalem, and the customers are my wife, my cousin, and my father-in-law.
+
+daniel = Customer.new("Daniel", "Rein")
+jen = Customer.new("Jennifer","Foss_feig")
+daniel2 = Customer.new("Daniel", "Safran")
+yosi = Customer.new("Yosi", "Foss-Feig")
+
+pinati = Restaurant.new("Pinati")
+taami = Restaurant.new("Taami")
+rachmo = Restaurant.new("Rachmo")
+
+review1 = Review.new(daniel, pinati)
+review2 = Review.new(jen, pinati)
+review3 = Review.new(daniel2, pinati)
+review4 = Review.new(yosi, pinati)
+review5 = Review.new(jen, taami)
+review6 = Review.new(daniel, taami)
+review7 = Review.new(daniel2, taami)
+review8 = Review.new(yosi, rachmo)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
