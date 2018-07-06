1e85819fade551fdce09c53983ac0541b8efb221
github username <github email address>
2018-07-06 10:57:40 -0400

    done

--- solution.rb
+++ solution.rb
@@ -1,1 +1,85 @@
 # Please copy/paste all three classes into this file to submit your solution!
+class Review
+  @@all = []
+
+  attr_reader :customer, :restaurant
+  attr_accessor :content
+
+  def initialize(customer, restaurant, content)
+    @customer = customer
+    @restaurant = restaurant
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
+
+
+class Restaurant
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|restaurant| restaurant.name == name}
+  end
+
+  def reviews
+    Review.all.select {|review| review.restaurant == self.name}
+  end
+
+  def customers
+    reviews.map {|review| review.customer}
+  end
+
+end
+
+
+class Customer
+  @@all = []
+
+  attr_accessor :first_name, :last_name
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name  = last_name
+    @@all << self
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|customer| customer.full_name == name}
+  end
+
+  def self.find_all_by_first_name(name)
+    self.all.select {|customer| customer.first_name == name}
+  end
+
+  def self.all_names
+    self.all.map {|customer| customer.full_name}
+  end
+
+  def add_review(restaurant, content)
+    Review.new(self, restaurant, content)
+  end
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,17 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+lin = Customer.new("Maneepailin", "Sriuthenchai")
+graham = Customer.new("Graham", "Troyer-Joy")
+graham2 = Customer.new("Graham", "Whoever")
+elbin = Customer.new("Elbin", "Keeppanasseril")
+
+olive_garden = Restaurant.new("Olive Garden")
+papa_johns = Restaurant.new("Papa Johns")
+wendys = Restaurant.new("Wendys")
+
+review1 = lin.add_review("Wendys", "good salad")
+review2 = graham.add_review("Wendys", "slow")
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
