a4d10b3e1949201c1a9122f5102cd929fd2465ac
yunglleung1 <yungl.leung1@gmail.com>
2018-07-06 11:02:03 -0400

    Done.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,48 @@
 class Customer
+  @@all = []
   attr_accessor :first_name, :last_name
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def self.all #should return **all** of the customer instances
+    @@all
+  end
+
+  def self.find_by_name(name) #given a string of a **full name**, returns the **first customer** whose full name matches
+    Review.all.find do |review|
+      if review.customer.full_name == name
+        review.customer.full_name #stack level too deep
+      end
+    end
+  end
+
+  def self.find_all_by_first_name(name)#given a string of a first name, returns an **array** containing all customers with that first name
+    Review.all.collect do |review|
+      if review.customer.first_name == name
+        review.customer.first_name #stack level too deep
+      end
+    end
+  end
+
+  def self.all_names#should return an **array** of all of the customer full names
+    Review.all.collect do |review|
+        review.customer.full_name #stack level too deep
+      end
+    end
+  end
+
+  def add_review(restaurant, content)
+    #given a **restaurant object** and some review content (as a string),
+    #creates a new review
+    review = Review.new(review, self, restaurant)
+    #associates it with that customer and restaurant. A `Review` belongs to a `Customer` and belongs to a `Restaurant`
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,24 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
-
+yung = Customer.new("Yung", "Leung")
+lin = Customer.new("Lin", "Something")
+applebees = Restaurant.new("Apple Bees")
+five_stars = Review.new("Five Stars", yung, applebees)
+two_stars = Review.new("Two Stars", lin, applebees)
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
+
+#Notes
+# Object Relations Code Challenge
+# Yelp-style domain
+# 3 three models: `Restaurant`, `Customer`, `Review`.
+#
+# Restaurant: has many Reviews
+# Customer: has many Reviews
+# Review(single source truth): belongs to a Customer` & to a Restaurant
+#
+# Restaurant : Customer: M : M
+#
+# goal: build out all methods listed in deliverables
+# use higher-level array methods such as `map`, `select` & `find` when appropriate in place of `each`
